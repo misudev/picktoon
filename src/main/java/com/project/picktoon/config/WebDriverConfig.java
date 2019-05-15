a103ddb64c54275d4db82e2446e11c33f3c3803f
misudev <jungmisu801@gmail.com>
2019-05-15 14:38:55 +0900

    설정 수정..

--- src/main/java/com/project/picktoon/config/WebDriverConfig.java
+++ src/main/java/com/project/picktoon/config/WebDriverConfig.java
@@ -36,6 +36,7 @@ public class WebDriverConfig {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
+        options.addArguments("--remote-debugging-port=9222");
         options.addArguments("window-size=1920x1080");
         options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
         options.addArguments("--no-sandbox");
